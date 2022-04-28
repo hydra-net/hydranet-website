6305dd303d01adf4117fce0e4c37e93cdf75311b
Sadhill94 <sadhill.dev@gmail.com>
2022-04-29 00:07:33 +0200

    refactor: restore dex download

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,6 +7,8 @@ export const HYDRANET_MEDIUM_FETCH_URL =
   'https://aiutsa6z6b.execute-api.us-east-1.amazonaws.com/default/hydranet-website-fetch-medium';
 
 export const DEX_DOWNLOAD_LINKS: Record<string, string> = {
-  windows: '',
-  linux: '',
+  windows:
+    'https://hydranet.s3.eu-central-1.amazonaws.com/light-wallet/staging/win/installer/Hydranet.zip',
+  linux:
+    'https://hydranet.s3.eu-central-1.amazonaws.com/light-wallet/staging/linux/installer/Hydranet.zip',
 };
