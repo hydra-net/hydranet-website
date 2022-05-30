74d5dfb8eba1edb9766a28ccb512bf313f07df5e
Sadhill94 <sadhill.dev@gmail.com>
2022-05-30 16:34:33 +0200

    bug project

---
+++ src/components/unshapped/testimonials.ts
@@ -0,0 +1,33 @@
+type Testimonial = {
+    quote: Array<string>;
+    author: string;
+    imageSrc: string;
+}
+
+
+const TESTIMONIALS: Array<Testimonial> = [
+    {
+        quote: [
+            "We should put resources toward a proper (trustless, serverless) ETH <-> BTC decentralized exchange.",
+            "It's embarrassing that we still can't easily move between the two largest crypto ecosystems trustlessly."
+        ],
+        author: "Vitalik Buterin",
+        imageSrc: "https://www.justetf.com/images/thumbnails/etf-investment-guide-theme-ethereum.jpg"
+    },
+    {
+        quote: [
+            "We should put resources toward a proper (trustless, serverless) ETH <-> BTC decentralized exchange.",
+            "It's embarrassing that we still can't easily move between the two largest crypto ecosystems trustlessly."
+        ],
+        author: "BUGON HJOAKJDS",
+        imageSrc: "https://www.justetf.com/images/thumbnails/etf-investment-guide-theme-ethereum.jpg"
+    },
+    {
+        quote: [
+            "We should put resources toward a proper (trustless, serverless) ETH <-> BTC decentralized exchange.",
+            "It's embarrassing that we still can't easily move between the two largest crypto ecosystems trustlessly."
+        ],
+        author: "REIIAKDJ ASDAsd ",
+        imageSrc: "https://www.justetf.com/images/thumbnails/etf-investment-guide-theme-ethereum.jpg"
+    }
+]
