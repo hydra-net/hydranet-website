19629567376f727a3e159a905a3587eefa472893
Sadhill94 <sadhill.dev@gmail.com>
2022-06-28 16:50:13 +0200

    add layout index page and create different navigations

--- src/components/Templates/Layout.tsx
+++ src/components/Templates/Layout.tsx
@@ -6,13 +6,18 @@ import { INavigationLink } from '../../interfaces';
 type LayoutProp = {
   children: ReactNode;
   navigation: INavigationLink[];
+  footerBgClass?: string;
 };
-const Layout = ({ children, navigation }: LayoutProp) => {
+const Layout = ({
+  children,
+  navigation,
+  footerBgClass = 'bg-brand-darker-blue',
+}: LayoutProp) => {
   return (
     <div id={'app-top'} className={'relative'}>
       <Navbar navigation={navigation} />
       <main>{children}</main>
-      <div className="bg-brand-darker-blue">
+      <div className={footerBgClass}>
         <BrandFooter />
       </div>
     </div>
