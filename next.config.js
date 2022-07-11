8e7045d823f5c943c04431896652735602b59376
Sadhill94 <sadhill.dev@gmail.com>
2022-07-12 01:27:51 +0200

    expose .env variable through nextjs, update .env.example

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {},
+  env: {
+    NEXT_DEFAULT_APP_LANGUAGE: process.env.NEXT_DEFAULT_APP_LANGUAGE || 'en',
+    NEXT_PUBLIC_GOOGLE_ANALYTICS_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
+    NEXT_STORYBLOK_ACCESS_TOKEN: process.env.NEXT_STORYBLOK_ACCESS_TOKEN,
+    NEXT_STORYBLOK_ACCESS_TOKEN_DEV:
+      process.env.NEXT_STORYBLOK_ACCESS_TOKEN_DEV,
+  },
 };
 
 module.exports = nextConfig;
